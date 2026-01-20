579b541871c77355ecd2c75689eea743119d844e
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-20 15:52:18 -0300

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 import icon from 'astro-icon'
+import sitemap from '@astrojs/sitemap'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://ravimedia.com.br',
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
